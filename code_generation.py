bed77721f1317ea832e3d20be376739e49046128
Plinio Moreno <plinio@isr.tecnico.ulisboa.pt>
2015-07-23 22:43:59 +0100

    added output code directory name as a parameter Now the .h files from yarpidl_rosmsgs are generated automatically Next: generate the skeleton code

--- code_generation.py
+++ code_generation.py
@@ -17,7 +17,8 @@ def process_input():
     parser.add_argument('-if','--input', type = argparse.FileType('r'), help='Text file that contains the action file (-af) and the directory (-ad)')
     parser.add_argument("-v", "--verbose", action="store_true")
     parser.add_argument('-af','--action_file', help='ROS-formatted action file')
-    parser.add_argument('-ad','--action_dir', help='Directory where .msg files will be stored')
+    parser.add_argument('-ad','--action_dir', help='Relative directory where .msg files will be stored')
+    parser.add_argument('-cd','--code_dir', help='Relative directory where the C++ code files will be stored')
     args = parser.parse_args()
     if args.input:
         if args.verbose:
@@ -26,20 +27,24 @@ def process_input():
         config.readfp(args.input)
         action_file_str = config.get('main','action_file')
         action_dir_str = config.get('main', 'action_dir')
+        code_dir_str = config.get('main', 'code_dir')
         if args.verbose:
             print 'Action file: ' + action_file_str
-            print 'Directory to store the msg files: ' + action_dir_str
+            print 'Relative directory to store the msg files: ' + action_dir_str
+            print 'Relative directory to store the generated code: ' + code_dir_str
 #    elif args.input and (not args.verbose):
 #        config = RawConfigParser()
 #        config.readfp(args.input)
 #        action_file_str = config.get('main','action_file')
 #        action_dir_str = config.get('main', 'action_dir')
-    elif args.action_file and args.action_dir:
+    elif args.action_file and args.action_dir and args.code_dir:
         action_file_str = args.action_file
         action_dir_str = args.action_dir
+        action_dir_str = args.code_dir        
         if args.verbose:
             print 'Action file: ' + action_file_str
-            print 'Directory to store the msg files: ' + action_dir_str
+            print 'Relative directory to store the msg files: ' + action_dir_str
+            print 'Relative directory to store the generated code: ' + code_dir_str
     else:
         print 'Missing arguments for proper execution'
         parser.print_help()
@@ -47,19 +52,22 @@ def process_input():
         action_dir_str=''
     try:
         absolute_path_fn = os.path.dirname(__file__) + '/actions/' + action_file_str
-        absolute_dir_name = os.path.dirname(__file__) + '/' + action_dir_str
+        absolute_action_dir_name = os.path.dirname(__file__) + '/' + action_dir_str
+        absolute_code_dir_name = os.path.dirname(__file__) + '/' + code_dir_str
         print absolute_path_fn
-        print absolute_dir_name
+        print absolute_action_dir_name
+        print absolute_code_dir_name
         my_temp_fp = open(absolute_path_fn)
         my_temp_fp.close()
     except IOError:
         print 'Action file does not exist'
-    return {'file_name': absolute_path_fn, 'dir_name': absolute_dir_name}
+    return {'file_name': absolute_path_fn, 'action_dir_name': absolute_action_dir_name, 'rel_action_dir_name': action_dir_str,'code_dir_name': absolute_code_dir_name,'rel_code_dir_name': code_dir_str}
 #        for line in args.input:
 #            print line,
 if __name__ == "__main__":
    absolute_io_inputs = process_input()
    action_code_generator = action_gen(absolute_io_inputs)
    action_code_generator.generate_mgs_files()
+   action_code_generator.generate_buffered_port_files()
    #Run the following command to get the .msg files
    #rosrun actionlib_msgs genaction.py Fibonacci.action -o ./msg
--- src/yarp_action_lib_server_generator.py
+++ src/yarp_action_lib_server_generator.py
@@ -6,15 +6,33 @@ This temporary script file is located here:
 /home/plinio/.spyder2/.temp.py
 """
 import subprocess
+import os
+import sys
 
 class yarp_action_lib_server_generator:
 
     def __init__(self,io_inputs):
         self.file_name = io_inputs['file_name']
-        self.directory_name = io_inputs['dir_name']
+        self.action_directory_name = io_inputs['action_dir_name']
+        self.relative_action_dir_name = io_inputs['rel_action_dir_name']
+        self.code_directory_name = io_inputs['code_dir_name']
+        self.relative_code_dir_name = io_inputs['rel_code_dir_name']
+#        self.absolute_path = io_inputs['path']
         
     def generate_mgs_files(self):
         #call python script
-        subprocess.call(['rosrun actionlib_msgs genaction.py ' + self.file_name + ' -o ' + self.directory_name], shell=True)
-        print 'rosrun actionlib_msgs genaction.py ' + self.file_name + ' -o ' + self.directory_name
+        subprocess.call(['rosrun actionlib_msgs genaction.py ' + self.file_name + ' -o ' + self.action_directory_name], shell=True)
+        print 'rosrun actionlib_msgs genaction.py ' + self.file_name + ' -o ' + self.action_directory_name
         #rosrun actionlib_msgs genaction.py Fibonacci.action -o ./msg
+    
+    def generate_buffered_port_files(self):
+        #For all the files in the folder
+        for root, dirs, files in os.walk(self.action_directory_name):
+            for name in files:
+                print name, root
+                origWD = os.getcwd() # remember our original working directory
+                os.chdir(os.path.join(os.path.abspath(sys.path[0]), self.relative_action_dir_name))
+                subprocess.Popen(['yarpidl_rosmsg','--out ' , self.code_directory_name + '/include ' ,  name]) 
+                os.chdir(origWD) # get 
+                #subprocess.call(['yarpidl_rosmsg --out ' + self.code_directory_name + '/include ' + ' ./' + self.relative_action_dir_name + '/' + name], shell=True)
+                #print ['yarpidl_rosmsg --out ' + './' + self.code_directory_name + '/include ' + ' ./' + self.relative_action_dir_name + '/' + name]
